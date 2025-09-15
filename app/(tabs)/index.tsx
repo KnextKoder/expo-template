8a56aefa450f44d39a4ea212ed20b6f9e48bcd3f
knextkoder <knextkoderklan@gmail.com>
2025-09-15 21:39:56 +0100

    Configured nativewind + typescript

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Text } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
@@ -74,6 +74,9 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
+      <Text className='text-red-500'>
+        Marvel Was here baby
+      </Text>
     </ParallaxScrollView>
   );
 }
